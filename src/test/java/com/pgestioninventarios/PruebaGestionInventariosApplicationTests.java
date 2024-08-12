7776ba0f9c2d52926b096dcade558a2933ae4950
JcromeroP <romeropascuasj@gmail.com>
2024-08-12 10:14:08 -0500

    Creación del proyecto

---
+++ src/test/java/com/pgestioninventarios/PruebaGestionInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgestioninventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGestionInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
